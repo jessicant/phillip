4e827df28e0a7dff09c6850f977c6f4b275a2bc8
Jessica Hamilton <jhamilton159@outlook.com>
2021-08-15 08:29:29 -0700

    THANK U MICHEAL

--- phillip.js
+++ phillip.js
@@ -1,40 +1,75 @@
-var totalFed = 0;
-var totalPats = 0;
-var sleeping = false;
+myStorage = window.localStorage;
+var totalFed;
+var totalPats;
+inboundStorageProcessing();
 
-function readTextFile(){
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", "file://C:\Users\Jess\Desktop\Programmin\Projects\Pet\testing.txt");
-  var allText = rawFile.responseText;
-  console.log(allText);
+function inboundStorageProcessing(){
+  totalFed = myStorage.getItem('totalFed');
+  totalPats = myStorage.getItem('totalPats');
+  sleeping = myStorage.getItem('sleeping');
+
+  if (totalFed == null){
+    totalFed = '0';
+    myStorage.setItem('totalFed', totalFed);
+  }
+  if (totalPats == null){
+    totalPats = '0';
+    myStorage.setItem('totalPats', totalPats);
+  }
+  if (sleeping == null){
+    sleeping = 'f';
+    myStorage.setItem('sleeping', sleeping);
+  }
+  totalFed = parseInt(totalFed);
+  totalPats = parseInt(totalPats);
+  if (sleeping == 'f'){
+    sleeping = false;
+  } else if (sleeping == 't'){
+    sleeping = true;
+  }
 }
 
 function feed(amount){
   totalFed += amount;
   var health = document.getElementById("health");
-  health.textContent = totalFed/4 + " HP ";
-  console.log("what up");
+  health.innerText = totalFed/4 + " HP ";
+  outboundStorageProcessing();
 }
 
 function sleepingIcon() {
   if (sleeping){
     sleeping = false;
-  } else {
+  } else if (!sleeping) {
     sleeping = true;
   }
   var icon = document.getElementById("icon");
   if(!sleeping){
-   icon.innerText = "(●´ω｀●)"
+   icon.innerText = "(●´ω｀●)";
  } else {
     icon.innerText = "(︶｡︶✽)"
  }
+ outboundStorageProcessing();
+
 }
 
-function pat(){
+function pat(amount){
   var icon = document.getElementById("icon");
   var pats = document.getElementById("petting");
-  totalPats += 1;
+  totalPats += amount;
+  outboundStorageProcessing();
   pats.innerText = totalPats + " Pats";
-  icon.innerText = "≧◡≦";
+  if (amount > 0){
+    icon.innerText = "≧◡≦";
+  }
   setTimeout(() => {icon.innerText = "(●´ω｀●)"}, 2000);
+}
+
+function outboundStorageProcessing(){
+  myStorage.setItem('totalFed', totalFed.toString());
+  myStorage.setItem('totalPats', totalPats.toString());
+  if (sleeping){
+    myStorage.setItem('sleeping', 't');
+  } else if (!sleeping) {
+    myStorage.setItem('sleeping', 'f');
+  }
 }
