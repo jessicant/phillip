e1c4af850d4b9849879d7fb886c0992fe8178089
Jessica Hamilton <jhamilton159@outlook.com>
2021-08-19 12:57:14 -0700

    August update 0.08

--- phillip.js
+++ phillip.js
@@ -2,10 +2,17 @@ myStorage = window.localStorage;
 var totalFed;
 var totalPats;
 var sleeping;
-loadStateFromLocalStorage();
+var sad;
+var angry;
+var happy;
+var HP;
+var happiness;
+var img;
 
 function init(){
+  img = document.getElementById("phillipImage");
   loadStateFromLocalStorage();
+  var time = getSystemTime(); 
   feed(0);
   patThePhillip(0);
 }
@@ -31,6 +38,11 @@ function loadStateFromLocalStorage(){
     sleeping = false;
     myStorage.setItem('sleeping', sleeping);
   }
+  if (sleeping != null && sleeping.charAt(0) == 't'){
+    sleeping = true;
+  } else {
+    sleeping = false;
+  }
   totalFed = parseInt(totalFed);
   totalPats = parseInt(totalPats);
 }
@@ -38,7 +50,19 @@ function loadStateFromLocalStorage(){
 function feed(amount){
   totalFed += amount;
   var health = document.getElementById("health");
-  health.innerText = totalFed/4 + " HP ";
+  HP = totalFed / 4;
+  if (HP < 0){
+    HP = 0;
+  } else if (HP > 100){
+    HP = 100
+  }
+  if (HP < 25) {
+    angry = true;
+  } else {
+    angry = false;
+  }
+  health.innerText = HP + " HP ";
+  toggleDisplay();
   outboundStorageProcessing();
 }
 
@@ -48,33 +72,57 @@ function toggleSleep() {
   } else if (!sleeping) {
     sleeping = true;
   }
-  var icon = document.getElementById("icon");
-  if(!sleeping){
-   icon.innerText = "(●´ω｀●)";
- } else {
-    icon.innerText = "(︶｡︶✽)"
- }
- outboundStorageProcessing();
-
+  outboundStorageProcessing();
+  toggleDisplay();
 }
 
 function patThePhillip(amount){
-  var pats = document.getElementById("petting");
+  var happinessDisplay = document.getElementById("happiness");
   totalPats += amount;
-  pats.innerText = totalPats + " Pats";
+  if (totalPats < 0){
+    totalPats = 0;
+  } else if (totalPats > 200){
+    totalPats = 200;
+  }
+  happiness = totalPats/2;
+  happinessDisplay.innerText = happiness + " happiness";
   outboundStorageProcessing();
   if (amount > 0){
-    toggleHappy(document.getElementById("icon"));
+    happy = true;
   }
-}
-
-function toggleHappy(icon){
-  icon.innerText = "≧◡≦";
-  setTimeout(() => {icon.innerText = "(●´ω｀●)"}, 2000);
+  if (happiness < 25){
+    sad = true;
+  } else {
+    sad = false;
+  }
+  toggleDisplay();
 }
 
 function outboundStorageProcessing(){
   myStorage.setItem('totalFed', totalFed);
   myStorage.setItem('totalPats', totalPats);
-  myStorage.setItem('sleep', sleeping);
+  myStorage.setItem('sleeping', sleeping);
   }
+
+function hourPassage(hours){
+  var decreaseHunger = -10 * hours;
+  var decreaseHappiness = -20 * hours;
+  feed(decreaseHunger);
+  patThePhillip(decreaseHappiness);
+}
+
+function toggleDisplay(){
+  if (happy) {
+    img.src = "assets/happyPhillip.png"
+    happy = false;
+    setTimeout(() => {toggleDisplay()}, 2000);
+  } else if (sleeping){
+    img.src = "assets/sleepingPhillip.png"
+  } else if (sad) {
+    img.src = "assets/sadPhillip.png"
+  } else if (angry) {
+    img.src = "assets/angryPhillip.png"
+  } else {
+    img.src = "assets/defaultPhillip.png"
+  }
+}
