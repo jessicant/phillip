4c4ee046e68964b836ea2f17d107b6557b509a5e
jhamilton159 <61167311+jhamilton159@users.noreply.github.com>
2021-08-19 14:26:45 -0700

    THANK U MICHEAL (#1)
    * THANK U MICHEAL
    
    * THANK U MICHEAL
    
    * August update 0.08
    
    * August Updates 0.1
    
    * August Updates 0.11

--- phillip.js
+++ phillip.js
@@ -1,40 +1,127 @@
-var totalFed = 0;
-var totalPats = 0;
-var sleeping = false;
+myStorage = window.localStorage;
+var totalFed;
+var totalPats;
+var sleeping;
+var sad;
+var angry;
+var happy;
+var HP;
+var happiness;
+var img;
 
-function readTextFile(){
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", "file://C:\Users\Jess\Desktop\Programmin\Projects\Pet\testing.txt");
-  var allText = rawFile.responseText;
-  console.log(allText);
+function init(){
+  img = document.getElementById("phillipImage");
+  loadStateFromLocalStorage(); 
+  feed(0);
+  patThePhillip(0);
+}
+/*This functions loads the state of phillip from the computer's local storage.
+The variables pulled from local storage are all strings. If the variables do not exist,
+i.e. there is no record of a phillip on this machine, variables will be created.
+Then the variables are parsed into their correct forms.
+*/
+function loadStateFromLocalStorage(){
+  totalFed = myStorage.getItem('totalFed');
+  totalPats = myStorage.getItem('totalPats');
+  sleeping = myStorage.getItem('sleeping');
+
+  if (totalFed == null){
+    totalFed = 0;
+    myStorage.setItem('totalFed', totalFed);
+  }
+  if (totalPats == null){
+    totalPats = 0;
+    myStorage.setItem('totalPats', totalPats);
+  }
+  if (sleeping == null){
+    sleeping = false;
+    myStorage.setItem('sleeping', sleeping);
+  }
+  if (sleeping != null && sleeping.charAt(0) == 't'){
+    sleeping = true;
+  } else {
+    sleeping = false;
+  }
+  totalFed = parseInt(totalFed);
+  totalPats = parseInt(totalPats);
 }
 
 function feed(amount){
   totalFed += amount;
   var health = document.getElementById("health");
-  health.textContent = totalFed/4 + " HP ";
-  console.log("what up");
+  HP = totalFed / 4;
+  if (HP < 0){
+    HP = 0;
+  } else if (HP > 100){
+    HP = 100
+  }
+  if (HP < 25) {
+    angry = true;
+  } else {
+    angry = false;
+  }
+  health.innerText = HP + " HP ";
+  toggleDisplay();
+  outboundStorageProcessing();
 }
 
-function sleepingIcon() {
+function toggleSleep() {
   if (sleeping){
     sleeping = false;
-  } else {
+  } else if (!sleeping) {
     sleeping = true;
   }
-  var icon = document.getElementById("icon");
-  if(!sleeping){
-   icon.innerText = "(●´ω｀●)"
- } else {
-    icon.innerText = "(︶｡︶✽)"
- }
+  outboundStorageProcessing();
+  toggleDisplay();
 }
 
-function pat(){
-  var icon = document.getElementById("icon");
-  var pats = document.getElementById("petting");
-  totalPats += 1;
-  pats.innerText = totalPats + " Pats";
-  icon.innerText = "≧◡≦";
-  setTimeout(() => {icon.innerText = "(●´ω｀●)"}, 2000);
+function patThePhillip(amount){
+  var happinessDisplay = document.getElementById("happiness");
+  totalPats += amount;
+  if (totalPats < 0){
+    totalPats = 0;
+  } else if (totalPats > 200){
+    totalPats = 200;
+  }
+  happiness = totalPats/2;
+  happinessDisplay.innerText = happiness + " happiness";
+  outboundStorageProcessing();
+  if (amount > 0){
+    happy = true;
+  }
+  if (happiness < 25){
+    sad = true;
+  } else {
+    sad = false;
+  }
+  toggleDisplay();
+}
+
+function outboundStorageProcessing(){
+  myStorage.setItem('totalFed', totalFed);
+  myStorage.setItem('totalPats', totalPats);
+  myStorage.setItem('sleeping', sleeping);
+  }
+
+function hourPassage(hours){
+  var decreaseHunger = -10 * hours;
+  var decreaseHappiness = -20 * hours;
+  feed(decreaseHunger);
+  patThePhillip(decreaseHappiness);
+}
+
+function toggleDisplay(){
+  if (happy) {
+    img.src = "assets/happyPhillip.png"
+    happy = false;
+    setTimeout(() => {toggleDisplay()}, 2000);
+  } else if (sleeping){
+    img.src = "assets/sleepingPhillip.png"
+  } else if (sad) {
+    img.src = "assets/sadPhillip.png"
+  } else if (angry) {
+    img.src = "assets/angryPhillip.png"
+  } else {
+    img.src = "assets/defaultPhillip.png"
+  }
 }
